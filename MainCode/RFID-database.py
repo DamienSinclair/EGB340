869d932f5f3d6b5946dfe47a099e08b73d3ed565
DamienSinclair <damien.l.sinclair@gmail.com>
2017-05-04 01:38:23 +1000

    Main Code working

--- MainCode/RFID-database.py
+++ MainCode/RFID-database.py
@@ -1,46 +1,58 @@
-# How the modules were setup
-# from libRFID import *
-# import Adafruit_PN532 as PN532
-# import MySQLdb
-# from LibMySQL import *
-# import time
-# import sys
-# import os
 
 import sys
 
+#import RFID Libs
+import Adafruit_PN532 as PN532
 sys.path.append('/home/pi/Desktop/EGB340/RFID')
 from libRFID import *
-import Adafruit_PN532 as PN532
 
-sys.path.append('/home/pi/Desktop/EGB340/Database')
+#Import Database Libs
 import MySQLdb
-from LibMySQL import *
+sys.path.append('/home/pi/Desktop/EGB340/Database')
+from LibMySQL import*
+
 
 #Configure the Reader
 pn532 = initialise_RFID(18, 25, 23, 24)
 
 while True :
+    # Get the user to scan to borrow the Trolly
+    print "Please scan your card to borrow a trolly"
 
     #Read Current Value
     CardID = read(pn532)
 
     Userdata = queryCardID(CardID)
 
-    fname = Userdata[0]
-    sname = Userdata[1]
+    # the card is not registered to a user
+    if not Userdata:
+        print "You dont have an account with use"
+        print "Would you like to set one up and speed up your shopping experience today?"
 
-    print "Hello", (fname, sname)
+        UserInput = raw_input()
 
-    print "scan a product"
+        if UserInput == 'yes':
+            print"sweet as bro"
+            end
 
-    while CardID != None:
+    # The card has a user
+    else:
+        fname = Userdata[0]
+        sname = Userdata[1] 
+        print('Hi %s') %fname
 
-        ProductID = read(pn532)
+        print "You're good to go"
+        print "Start scanning products"
 
-        ProductData = queryProductID(ProductID)
+        k = 1
+        
+        while k==1:
 
-        pname = ProductData[0]
-        pprice = ProductData[1]
+            ProductID = read(pn532)
 
-        print "shit", (pname, pprice)
+            ProductData = queryProductID(ProductID)
+
+            pname = ProductData[0]
+            pprice = ProductData[1]
+
+            print("%s Costs %s" % (pname, pprice))
